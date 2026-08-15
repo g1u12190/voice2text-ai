41ae3c6f9944c9f1777fc801cbd1fa0b5fbe6d03
g1u12190 <129238890+g1u12190@users.noreply.github.com>
2026-08-14 21:30:22 -0500

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -33,7 +33,7 @@ async function improveText(){
 
         const response =
         await fetch(
-            "http://127.0.0.1:8000/improve",
+            "https://voice2text-ai-backend.onrender.com",
             {
 
                 method:"POST",
