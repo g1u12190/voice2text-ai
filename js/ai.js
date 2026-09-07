b92696ff37ca13d461b5e3a31574b5669ab2b37f
g1u12190 <129238890+g1u12190@users.noreply.github.com>
2026-09-07 18:33:42 -0500

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -11,6 +11,19 @@ document.getElementById("autoImprove");
 
 //------------------------------------------------------
 
+//======================================================
+// Despertar backend al abrir la aplicación
+//======================================================
+
+fetch("https://voice2text-ai-backend.onrender.com/")
+    .then(() => {
+        console.log("Backend listo");
+    })
+    .catch(() => {
+        console.log("Backend iniciando...");
+    });
+//------------------------------------------------------
+
 async function improveText(){
 
     const text = output.value.trim();
